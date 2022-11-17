f4f6518d9b6bbe9b7912df58e2f55ff3db3495fc
AnjaSamsom <anjavt14@gmail.com>
2022-11-17 17:50:32 -0500

    I made input for username and password

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
+# run this with flask --app app run
 @app.route("/", methods=['GET', 'POST'])
 def home():
     # Home page
